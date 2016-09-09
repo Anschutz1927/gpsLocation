15d1ce2c984e5fbc0bf74815873b88767e1fd988
Anschutz1927 <blackpearl_2007@tut.by>
2016-09-09 21:19:26 +0300

    2 (interface work with bd) <esc> :wq <enter>

--- app/src/main/java/by/black_pearl/cheloc/DataBaser.java
+++ app/src/main/java/by/black_pearl/cheloc/DataBaser.java
@@ -30,11 +30,14 @@ public class DataBaser extends SQLiteOpenHelper {
         Log.i(LOG_TAG, "DataBaser");
     }
 
+<<<<<<< HEAD
     private void insertFirstLocation(SQLiteDatabase db) {
         Log.i(LOG_TAG, "insertFirstLocation");
         inserNewAddress("Энергосбыт", "53.923269","27.596573", "203");
     }
 
+=======
+>>>>>>> da794f8... 2 (interface work with bd)
     public void inserNewAddress(String address, String latitude,
                                 String longtitude, String altitude) {
         try {
@@ -94,7 +97,20 @@ public class DataBaser extends SQLiteOpenHelper {
                 LATITUDE_COLUMN + " text, " +
                 LONGTITUDE_COLUMN + " text, " +
                 ALTITUDE_COLUMN + " text);");
-        insertFirstLocation(db);
+        try {
+            Log.i(LOG_TAG, "inserNewAddress");
+            ContentValues cv = new ContentValues();
+            cv.put(ADDRESS_COLUMN, "Энергосбыт");
+            cv.put(LATITUDE_COLUMN, "53.923269");
+            cv.put(LONGTITUDE_COLUMN, "27.596573");
+            cv.put(ALTITUDE_COLUMN, "203");
+            db.insert(TABLE_NAME, null, cv);
+            db.close();
+            Toast.makeText(context, "Была создана новая БД приложения.", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(context, "Создать БД не удалось!", Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
