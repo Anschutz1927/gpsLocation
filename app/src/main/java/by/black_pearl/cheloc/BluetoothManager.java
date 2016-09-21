01ca960b182eb5bd737ea2b720bc5fa1b70e4e3a
Anschutz1927 <anschutz1927@gmail.com>
2016-09-21 13:17:28 +0300

    2 (bt manager)

--- app/src/main/java/by/black_pearl/cheloc/BluetoothManager.java
+++ app/src/main/java/by/black_pearl/cheloc/BluetoothManager.java
@@ -1,7 +1,12 @@
 package by.black_pearl.cheloc;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.util.Log;
 
 import java.util.Set;
@@ -11,12 +16,36 @@ import java.util.Set;
  */
 public class BluetoothManager {
     private static final String LOG_TAG = "BluetoothManager";
+    private Activity activity;
+    private final BroadcastReceiver receiver;
+    private BluetoothAdapter bluetoothAdapter;
 
-    public BluetoothManager() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    public BluetoothManager(Activity activity) {
+        this.activity = activity;
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        printKnownBtDevicesToLog();
+        this.receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    Log.i(LOG_TAG, " \nmReceiver:onRecive");
+                    Log.i(LOG_TAG, "bt device address = " + device.getAddress());
+                    Log.i(LOG_TAG, "bt device name = " + device.getName());
+                }
+            }
+        };
+        registerReceiver();
+    }
+
+    private void printKnownBtDevicesToLog() {
         if(bluetoothAdapter != null) {
-            Log.i(LOG_TAG, "bluetooth workaet.\n");
+            Log.i(LOG_TAG, "bluetooth workaet...\n");
             return;
+        }
+        else {
+            Log.i(LOG_TAG, "bluetooth NE workaet...\n");
         }
         if(bluetoothAdapter.isEnabled()) {
             Log.i(LOG_TAG, "bluetooth is enabled\n");
@@ -34,5 +63,22 @@ public class BluetoothManager {
             Log.i(LOG_TAG, "bluetooth is NOT enabled");
             return;
         }
+    }
+
+    private void registerReceiver() {
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        this.activity.registerReceiver(receiver, filter);
+    }
+
+    public void unregisterReceiver() {
+        this.activity.unregisterReceiver(receiver);
+    }
+
+    public void startSearchBtDevices() {
+        this.bluetoothAdapter.startDiscovery();
+    }
+
+    public void stopSearchBtDevices() {
+        this.bluetoothAdapter.cancelDiscovery();
     }
 }
